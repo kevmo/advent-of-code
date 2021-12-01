6139b86a792b311ecd6993cfff83bf8f22982bc4
Kevin Moore <k.james.moore@gmail.com>
2021-12-01 17:21:49 -0500

    Initiate node modules.

---
+++ 2021/day-01/depth_increases.js
@@ -0,0 +1,16 @@
+// import data
+
+// convert into array
+
+// instatiate counter
+
+// iterate over array
+
+// each time a value is more than previous one, increment counter
+
+
+import { readFileSync } from 'fs';
+
+const data = readFileSync('depths.dat');
+
+console.log(data)
